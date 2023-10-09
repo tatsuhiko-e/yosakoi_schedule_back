2189f85a88fbcb37c2c2933682bab101b73e87a2
tatsuhiko-e <70308705+tatsuhiko-e@users.noreply.github.com>
2023-10-09 14:41:55 +0900

    adminサインアップ・サインイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'Admin', at: "admin", controllers: {
+        registrations: 'api/mobile/user/registrations'
+      }
+      namespace :admin do
+        resources :sessions, only: %i[index]
+      end
+    end
     namespace :mobile do
       mount_devise_token_auth_for 'User', at: "user", controllers: {
         registrations: 'api/mobile/user/registrations'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_29_145354) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_050650) do
+  create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "email"
+    t.integer "area"
+    t.integer "sub_area"
+    t.boolean "is_active"
+    t.boolean "is_recruitment"
+    t.text "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
