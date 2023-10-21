93934889b5cf8401e7a4200cf9218c9fe99a68f7
tatsuhiko-e <70308705+tatsuhiko-e@users.noreply.github.com>
2023-10-21 16:10:22 +0900

    メール認証　success＿url未解決

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = true
-  config.action_mailer.default_url_options = { host: 'localhost:3001' } # 追加
-  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] } # 追加
+  config.action_mailer.default_url_options = { host: 'localhost:3000' } 
+  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] } 
+
+  config.action_mailer.delivery_method = :smtp
+
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
