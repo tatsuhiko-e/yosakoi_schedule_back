681e702ee3ac0a3399195a23c761da7600f1ef80
tatsuhiko-e <70308705+tatsuhiko-e@users.noreply.github.com>
2023-11-23 23:30:48 +0900

    電話番号ログインはできるが、サインインはできない

--- app/controllers/api/mobile/user/sessions_controller.rb
+++ app/controllers/api/mobile/user/sessions_controller.rb
@@ -4,12 +4,20 @@ module Api
   module V1
     module Mobile
       class SessionsController < ApplicationController
+        before_action :configure_sign_in_params, only: [:create]
+
         def index
           if current_api_v1_user
             render json: { is_login: true, data: current_api_mobile_user }
           else
             render json: { is_login: false, message: 'ユーザーが存在しません' }
           end
+        end
+
+        protected
+      
+        def configure_sign_in_params
+          devise_parameter_sanitizer.permit(:sign_in, keys: [:phone_number])
         end
       end
     end
